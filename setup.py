1a489f69034ceeb98d476ed4e19b7dd6a9894f0e
Benjamin Nigjeh <benjamin.nigjeh@gmail.com>
2024-05-16 02:07:13 -0700

    changes

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="backend",
+    version="0.0.1",
+    author="Ben Nigjeh",
+    author_email="benjamin.nigjeh@gmail.com",
+    install_requires=["pyteomics"],
+    packages=find_packages()
+)
